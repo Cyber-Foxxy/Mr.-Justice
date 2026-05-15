97c4c4dd002cccff13e6e993da8e28708deced45
Cyber-Foxxy <bluenosepittiezephyr2019@gmail.com>
2026-05-15 16:03:11 -0600

    color changes to lighten it

--- script.js
+++ script.js
@@ -49,8 +49,8 @@ class Player {
 
     // Lantern glow
    const glow = ctx.createRadialGradient(0, -48, 8, 0, -48, 170);
-    glow.addColorStop(0, "rgba(254, 252, 241, 0.8)"); 
-    glow.addColorStop(0.35, "rgba(242, 239, 233, 0.4)");
+    glow.addColorStop(0, "rgba(248, 237, 174, 0.26)"); 
+    glow.addColorStop(0.35, "rgba(242, 239, 233, 0)");
     glow.addColorStop(1, "rgba(239, 235, 231, 0)");
     ctx.fillStyle = glow;
     ctx.beginPath();
